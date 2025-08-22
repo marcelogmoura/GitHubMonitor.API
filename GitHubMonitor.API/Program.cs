43a19af0f2b271175ac50b691bb7d37237b027ac
marcelogmoura <mgoura@gmail.com>
2025-08-22 14:53:44 -0300

    fix: ajuste no appsettings, program e context

--- GitHubMonitor.API/Program.cs
+++ GitHubMonitor.API/Program.cs
@@ -4,8 +4,16 @@ using GitHubMonitor.Domain.Interfaces.Services;
 using GitHubMonitor.Domain.Services;
 using GitHubMonitor.Infra.Data.Contexts;
 using GitHubMonitor.Infra.Data.Repositories;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// **NOVA LINHA:** Registra o serializador do Guid como a primeira coisa
+BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+
+// Adiciona a configuração do MongoDB lendo do appsettings.json
 var mongoDBSettings = builder.Configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
 
 builder.Services.AddControllers();
@@ -16,7 +24,6 @@ builder.Services.AddHttpClient();
 
 builder.Services.AddScoped<IGitHubService, GitHubService>();
 builder.Services.AddScoped<IRepositoryRepository, RepositoryRepository>();
-
 builder.Services.AddSingleton<MongoDbContext>(_ => new MongoDbContext(mongoDBSettings.ConnectionString ?? string.Empty, mongoDBSettings.DatabaseName ?? string.Empty));
 
 
--- GitHubMonitor.Infra.Data/Contexts/MongoDbContext.cs
+++ GitHubMonitor.Infra.Data/Contexts/MongoDbContext.cs
@@ -1,8 +1,5 @@
 ﻿using GitHubMonitor.Domain.Entities;
 using MongoDB.Driver;
-using MongoDB.Driver.Core.Connections;
-using MongoDB.Driver.Core.Events;
-using System.Security.Authentication;
 
 namespace GitHubMonitor.Infra.Data.Contexts
 {
@@ -13,9 +10,7 @@ namespace GitHubMonitor.Infra.Data.Contexts
         public MongoDbContext(string connectionString, string databaseName)
         {
             var settings = MongoClientSettings.FromConnectionString(connectionString);
-
-            // Adiciona a configuração de ServerApi para garantir a compatibilidade
-            // com versões recentes do MongoDB.
+                        
             settings.ServerApi = new ServerApi(ServerApiVersion.V1);
 
             var client = new MongoClient(settings);
